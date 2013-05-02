3f06e400fdae3a13c412741ce8af8a5cd38b2832
Jared Hanson <jaredhanson@gmail.com>
2013-05-02 08:21:06 -0700

    Stub out test suite.

---
+++ test/index.test.js
@@ -0,0 +1,10 @@
+var htaccess = require('index');
+
+
+describe('htaccess plugin', function() {
+  
+  it('should export function', function() {
+    expect(htaccess).to.be.a('function');
+  });
+  
+});
